fa1f0540f4efdd4774f733bbd6b7aaadaf75ac29
filipe <filipefguilardi@gmail.com>
2025-07-15 13:22:35 -0300

    first commit

---
+++ pdf-parse-json-api/src/main/java/ffg/filipe/pdf_parse_json/PdfParseJsonApplication.java
@@ -0,0 +1,13 @@
+package ffg.filipe.pdf_parse_json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfParseJsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfParseJsonApplication.class, args);
+	}
+
+}
---
+++ pdf-parse-json-api/src/test/java/ffg/filipe/pdf_parse_json/PdfParseJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package ffg.filipe.pdf_parse_json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfParseJsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
